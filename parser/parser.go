46432e18124b0302d9c3b4e69bf2fc51fa99be49
Jason Kottler <jkottler@Honeydew.localdomain>
2017-04-24 10:34:01 +0100

    Add parsing functions.
    Tests pass. p.41, after code.

--- parser/parser.go
+++ parser/parser.go
@@ -29,5 +29,66 @@ func (p *Parser) nextToken() {
 }
 
 func (p *Parser) ParseProgram() *ast.Program {
-	return nil
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for p.curToken.Type != token.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+	return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.curToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	stmt := &ast.LetStatement{Token: p.curToken}
+
+	if !p.expectPeek(token.IDENT) {
+		return nil
+	}
+
+	stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+
+	if !p.expectPeek(token.ASSIGN) {
+		return nil
+	}
+
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) curTokenIs(tt token.TokenType) bool {
+	return p.tokenIs(p.curToken, tt)
+}
+
+func (p *Parser) peekTokenIs(tt token.TokenType) bool {
+	return p.tokenIs(p.peekToken, tt)
+}
+
+// couldn't stand the repetition of the above functions...
+func (p *Parser) tokenIs(tok token.Token, tt token.TokenType) bool {
+	return tok.Type == tt
+}
+
+func (p *Parser) expectPeek(tt token.TokenType) bool {
+	if p.peekTokenIs(tt) {
+		p.nextToken()
+		return true
+	} else {
+		return false
+	}
 }
